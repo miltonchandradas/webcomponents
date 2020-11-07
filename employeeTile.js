d27c9bfd1a90257a7a025f55459ec7b28caffdc8
Milton Chandradas <milton.chandradas@sap.com>
2020-11-06 19:05:42 -0500

    attributes + style bleeding...

--- employeeTile.js
+++ employeeTile.js
@@ -1,7 +1,16 @@
 class EmployeeTile extends HTMLElement {
    constructor() {
       super();
-      this.innerHTML = `John Doe`;
+
+      this.innerHTML = `<style>h3 {color:coral} </style><h3>${this.getAttribute(
+         "name"
+      )}</h3>`;
+
+      /* this.innerHTML = `<h3>${this.getAttribute("name")}</h3>`; */
+
+      /* this.innerHTML = `${this.getAttribute("name")}`; */
+
+      /* this.innerHTML = `Milton Chandradas`; */
    }
 }
 
